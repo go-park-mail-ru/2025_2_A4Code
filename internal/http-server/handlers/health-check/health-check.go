624f677c6e5732569c1d44c2ccee167cf4d75082
gettingthingsdoneee <wrjmll@gmail.com>
2025-10-17 00:39:56 +0300

    healthcheck and signup handlers changed

--- internal/http-server/handlers/health-check/health-check.go
+++ internal/http-server/handlers/health-check/health-check.go
@@ -1,14 +1,75 @@
 package health_check
 
 import (
-	"2025_2_a4code/internal/http-server/handlers"
-	ua "2025_2_a4code/internal/lib/session"
+	resp "2025_2_a4code/internal/lib/api/response"
+	"2025_2_a4code/internal/lib/session"
+
+	"encoding/json"
 	"net/http"
+	"time"
 )
 
-func (h *handlers.Handlers) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := ua.CheckSession(r, handlers.SECRET)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
+type HealthResponse struct {
+	Status    string            `json:"status"`
+	Timestamp string            `json:"timestamp"`
+	Services  map[string]string `json:"services,omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Body HealthResponse `json:"body,omitempty"`
+}
+
+type HealthCheckHandler struct {
+	secret []byte
+}
+
+func New(secret []byte) *HealthCheckHandler {
+	return &HealthCheckHandler{
+		secret: secret,
 	}
+}
+
+func (h *HealthCheckHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := session.CheckSession(r, h.secret)
+	if err != nil {
+		sendErrorResponse(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	healthStatus := HealthResponse{
+		Status:    "healthy",
+		Timestamp: time.Now().Format(time.RFC3339),
+		Services: map[string]string{
+			"api": "operational",
+		},
+	}
+
+	response := Response{
+		Response: resp.Response{
+			Status: http.StatusText(http.StatusOK),
+		},
+		Body: healthStatus,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&response)
+	if err != nil {
+		sendErrorResponse(w, "Внутренняя ошибка сервера", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func sendErrorResponse(w http.ResponseWriter, errorMsg string, statusCode int) {
+
+	response := Response{
+		Response: resp.Response{
+			Status: http.StatusText(statusCode),
+			Error:  errorMsg,
+		},
+	}
+
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(&response)
 }
--- internal/http-server/handlers/signup/signup.go
+++ internal/http-server/handlers/signup/signup.go
@@ -1,82 +1,112 @@
 package signup
 
 import (
-	handlers2 "2025_2_a4code/internal/http-server/handlers"
-	"2025_2_a4code/models"
+	"2025_2_a4code/internal/domain"
+	resp "2025_2_a4code/internal/lib/api/response"
+
+	profileUcase "2025_2_a4code/internal/usecase/profile"
 	"encoding/json"
 	"net/http"
-	"time"
-
-	"github.com/golang-jwt/jwt/v5"
 )
 
-func (h *handlers2.Handlers) SignupHandler(w http.ResponseWriter, r *http.Request) {
+type SignupResponse struct {
+	Status    string `json:"status"`
+	Timestamp string `json:"timestamp"`
+}
+
+type Response struct {
+	resp.Response
+	Body SignupResponse `json:"body,omitempty"`
+}
+
+type HandlerSignup struct {
+	profileUCase *profileUcase.ProfileUcase
+}
+
+func New(ucBP *profileUcase.ProfileUcase) *HandlerSignup {
+	return &HandlerSignup{profileUCase: ucBP}
+}
+
+func (h *HandlerSignup) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		http.Error(w, "Неправильный метод", http.StatusMethodNotAllowed)
+		sendErrorResponse(w, "Неправильный метод", http.StatusMethodNotAllowed)
 		return
 	}
 
-	var credentials models.RegisteredUser
+	var credentials domain.Profile
 	if err := json.NewDecoder(r.Body).Decode(&credentials); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		sendErrorResponse(w, "Неправильный запрос", http.StatusBadRequest)
 		return
 	}
 
-	// проверка логина
-	for _, user := range handlers2.users {
-		if credentials.Login == user["login"] {
-			http.Error(w, "Пользователь с таким логином уже существует", http.StatusUnauthorized)
-			return
-		}
+	// // проверка логина
+	// for _, user := range handlers2.users {
+	// 	if credentials.Login == user["login"] {
+	// 		http.Error(w, "Пользователь с таким логином уже существует", http.StatusUnauthorized)
+	// 		return
+	// 	}
+	// }
+
+	// newUser := map[string]string{
+	// 	"login":       credentials.Login,
+	// 	"password":    credentials.Password,
+	// 	"username":    credentials.Username,
+	// 	"dateofbirth": credentials.DateOfBirth,
+	// 	"gender":      credentials.Gender,
+	// }
+
+	// //записываем в мап
+	// handlers2.users = append(handlers2.users, newUser)
+
+	// // создаем токен
+	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+	// 	"login": credentials.Login,
+	// 	"exp":   time.Now().Add(24 * time.Hour).Unix(),
+	// })
+
+	// // подписываем
+	// session, err := token.SignedString(handlers2.SECRET)
+
+	// if err != nil {
+	// 	http.Error(w, "Ошибка регистрации", http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// cookie := &http.Cookie{
+	// 	Name:     "session_id",
+	// 	Value:    session,
+	// 	MaxAge:   3600,
+	// 	HttpOnly: true,
+	// 	Path:     "/",
+	// }
+
+	// // ставим куки
+	// http.SetCookie(w, cookie)
+
+	// w.Header().Set("Content-Type", "application/json")
+	// err = json.NewEncoder(w).Encode(map[string]any{
+	// 	"status": "200",
+	// 	"body": struct {
+	// 		Message string `json:"message"`
+	// 	}{"Пользователь зарегистрирован"},
+	// })
+
+	// if err != nil {
+	// 	http.Error(w, "Внутренняя ошибка сервера", http.StatusInternalServerError)
+	// 	return
+	// }
+}
+
+func sendErrorResponse(w http.ResponseWriter, errorMsg string, statusCode int) {
+
+	response := Response{
+		Response: resp.Response{
+			Status: http.StatusText(statusCode),
+			Error:  errorMsg,
+		},
 	}
 
-	newUser := map[string]string{
-		"login":       credentials.Login,
-		"password":    credentials.Password,
-		"username":    credentials.Username,
-		"dateofbirth": credentials.DateOfBirth,
-		"gender":      credentials.Gender,
-	}
-
-	//записываем в мап
-	handlers2.users = append(handlers2.users, newUser)
-
-	// создаем токен
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"login": credentials.Login,
-		"exp":   time.Now().Add(24 * time.Hour).Unix(),
-	})
-
-	// подписываем
-	session, err := token.SignedString(handlers2.SECRET)
-
-	if err != nil {
-		http.Error(w, "Ошибка регистрации", http.StatusInternalServerError)
-		return
-	}
-
-	cookie := &http.Cookie{
-		Name:     "session_id",
-		Value:    session,
-		MaxAge:   3600,
-		HttpOnly: true,
-		Path:     "/",
-	}
-
-	// ставим куки
-	http.SetCookie(w, cookie)
-
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(map[string]any{
-		"status": "200",
-		"body": struct {
-			Message string `json:"message"`
-		}{"Пользователь зарегистрирован"},
-	})
-
-	if err != nil {
-		http.Error(w, "Внутренняя ошибка сервера", http.StatusInternalServerError)
-		return
-	}
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(&response)
 }
