80de0b3a9342165fbc107ba71fd9c69625974cdf
gettingthingsdoneee <wrjmll@gmail.com>
2025-11-07 15:11:34 +0300

    driver changed

--- go.mod
+++ go.mod
@@ -16,7 +16,7 @@ require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/golang-migrate/migrate/v4 v4.19.0
 	github.com/golang/mock v1.6.0
-	github.com/lib/pq v1.10.9
+	github.com/jackc/pgx/v4 v4.18.3
 	github.com/minio/minio-go/v7 v7.0.95
 	github.com/stretchr/testify v1.10.0
 )
@@ -29,8 +29,17 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/minio/crc64nvme v1.0.2 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/philhofer/fwd v1.2.0 // indirect
--- internal/app/init.go
+++ internal/app/init.go
@@ -10,12 +10,12 @@ import (
 	messagepage "2025_2_a4code/internal/http-server/handlers/messages/message-page"
 	"2025_2_a4code/internal/http-server/handlers/messages/reply"
 	"2025_2_a4code/internal/http-server/handlers/messages/send"
-	"2025_2_a4code/internal/http-server/handlers/messages/sent"
+	"2025_2_a4code/internal/http-server/handlers/messages/threads"
 	profilepage "2025_2_a4code/internal/http-server/handlers/user/profile-page"
 	"2025_2_a4code/internal/http-server/handlers/user/settings"
 	uploadavatar "2025_2_a4code/internal/http-server/handlers/user/upload/upload-avatar"
+	uploadfile "2025_2_a4code/internal/http-server/handlers/user/upload/upload-file"
 	"2025_2_a4code/internal/http-server/middleware/cors"
-	csrfcheck "2025_2_a4code/internal/http-server/middleware/csrf-check"
 	"2025_2_a4code/internal/http-server/middleware/logger"
 	e "2025_2_a4code/internal/lib/wrapper"
 	avatarrepository "2025_2_a4code/internal/storage/minio/avatar-repository"
@@ -36,15 +36,16 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 const (
-	envLocal = "local"
-	envDev   = "dev"
-	envProd  = "prod"
+	FileUploadPath = "./files" // TODO: в дальнейшем будет минио
+	envLocal       = "local"
+	envDev         = "dev"
+	envProd        = "prod"
 )
 
 type Storage struct {
@@ -63,7 +64,6 @@ func Init() {
 
 	// Создание логгера
 	log := setupLogger(envLocal)
-	slog.SetDefault(log)
 	log.Debug("debug messages are enabled")
 	loggerMiddleware := logger.New(log)
 
@@ -80,15 +80,9 @@ func Init() {
 		log.Error(err.Error())
 	}
 
-	// Подключение MinIO
 	client, err := newMinioConnection(cfg.MinioConfig.Endpoint, cfg.MinioConfig.User, cfg.MinioConfig.Password, cfg.MinioConfig.UseSSL)
 	if err != nil {
 		log.Error("error connecting to minio")
-	}
-
-	err = bucketExists(client, cfg.MinioConfig.BucketName)
-	if err != nil {
-		log.Error("error checking bucket: " + err.Error())
 	}
 
 	// Создание репозиториев
@@ -99,49 +93,47 @@ func Init() {
 	// Создание юзкейсов
 	messageUCase := messageUcase.New(messageRepository)
 	profileUCase := profileUcase.New(profileRepository)
-	avatarUCase := avatarUcase.New(avatarRepository, profileRepository)
+	avatarUCase := avatarUcase.New(avatarRepository)
 
 	// Создание хэндлеров
-	loginHandler := login.New(profileUCase, SECRET)
-	signupHandler := signup.New(profileUCase, SECRET)
-	refreshHandler := refresh.New(SECRET)
-	logoutHandler := logout.New()
-	inboxHandler := inbox.New(messageUCase, avatarUCase, SECRET)
-	profileHandler := profilepage.New(profileUCase, avatarUCase, SECRET)
-	messagePageHandler := messagepage.New(messageUCase, avatarUCase, SECRET)
-	sendMessageHandler := send.New(messageUCase, SECRET)
-	settingsHandler := settings.New(profileUCase, SECRET)
-	replyHandler := reply.New(messageUCase, SECRET)
-	uploadAvatarHandler := uploadavatar.New(avatarUCase, profileUCase, SECRET)
-	sentHandler := sent.New(messageUCase, avatarUCase, SECRET)
+	loginHandler := login.New(profileUCase, log, SECRET)
+	signupHandler := signup.New(profileUCase, log, SECRET)
+	refreshHandler := refresh.New(log, SECRET)
+	logoutHandler := logout.New(log)
+	inboxHandler := inbox.New(profileUCase, messageUCase, log, SECRET)
+	profileHandler := profilepage.New(profileUCase, SECRET, log)
+	messagePageHandler := messagepage.New(profileUCase, messageUCase, SECRET, log)
+	sendMessageHandler := send.New(messageUCase, SECRET, log)
+	threadsHandler := threads.New(profileUCase, messageUCase, log, SECRET)
+	uploadFileHandler, err := uploadfile.New(FileUploadPath, log)
+	settingsHandler := settings.New(profileUCase, SECRET, log)
+	replyHandler := reply.New(messageUCase, SECRET, log)
+	uploadAvatarHanler := uploadavatar.New(avatarUCase, log, SECRET)
 
-	// настройка corsMiddleware
+	// настройка corsMiddlewares
 	corsMiddleware := cors.New()
-
-	// настройка csrfMiddleware
-	csrfMiddleware := csrfcheck.New()
 
 	slog.Info("Starting server...", slog.String("address", cfg.AppConfig.Host+":"+cfg.AppConfig.Port))
 
 	// роутинг + настройка middleware
-	http.Handle("/auth/login", (loggerMiddleware(corsMiddleware(http.HandlerFunc(loginHandler.ServeHTTP)))))
-	http.Handle("/auth/signup", (loggerMiddleware(corsMiddleware(http.HandlerFunc(signupHandler.ServeHTTP)))))
-	http.Handle("/auth/refresh", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(refreshHandler.ServeHTTP)))))
-	http.Handle("/auth/logout", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(logoutHandler.ServeHTTP)))))
-	http.Handle("/messages/inbox", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(inboxHandler.ServeHTTP)))))
-	http.Handle("/user/profile", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(profileHandler.ServeHTTP)))))
-	http.Handle("/messages/{message_id}", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(messagePageHandler.ServeHTTP)))))
-	http.Handle("/messages/send", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(sendMessageHandler.ServeHTTP)))))
-	http.Handle("/user/settings", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(settingsHandler.ServeHTTP)))))
-	http.Handle("/messages/reply", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(replyHandler.ServeHTTP)))))
-	http.Handle("/user/upload/avatar", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(uploadAvatarHandler.ServeHTTP)))))
-	http.Handle("/messages/sent", csrfMiddleware(loggerMiddleware(corsMiddleware(http.HandlerFunc(sentHandler.ServeHTTP)))))
+	http.Handle("/auth/login", loggerMiddleware(corsMiddleware(http.HandlerFunc(loginHandler.ServeHTTP))))
+	http.Handle("/auth/signup", loggerMiddleware(corsMiddleware(http.HandlerFunc(signupHandler.ServeHTTP))))
+	http.Handle("/auth/refresh", loggerMiddleware(corsMiddleware(http.HandlerFunc(refreshHandler.ServeHTTP))))
+	http.Handle("/auth/logout", loggerMiddleware(corsMiddleware(http.HandlerFunc(logoutHandler.ServeHTTP))))
+	http.Handle("/messages/inbox", loggerMiddleware(corsMiddleware(http.HandlerFunc(inboxHandler.ServeHTTP))))
+	http.Handle("/user/profile", loggerMiddleware(corsMiddleware(http.HandlerFunc(profileHandler.ServeHTTP))))
+	http.Handle("/messages/{message_id}", loggerMiddleware(corsMiddleware(http.HandlerFunc(messagePageHandler.ServeHTTP))))
+	http.Handle("/messages/send", loggerMiddleware(corsMiddleware(http.HandlerFunc(sendMessageHandler.ServeHTTP))))
+	http.Handle("/messages/threads", loggerMiddleware(corsMiddleware(http.HandlerFunc(threadsHandler.ServeHTTP))))
+	http.Handle("/upload/file", loggerMiddleware(corsMiddleware(http.HandlerFunc(uploadFileHandler.ServeHTTP))))
+	http.Handle("/user/settings", loggerMiddleware(corsMiddleware(http.HandlerFunc(settingsHandler.ServeHTTP))))
+	http.Handle("/messages/reply", loggerMiddleware(corsMiddleware(http.HandlerFunc(replyHandler.ServeHTTP))))
+	http.Handle("/upload/avatar", loggerMiddleware(corsMiddleware(http.HandlerFunc(uploadAvatarHanler.ServeHTTP))))
 
-	err = http.ListenAndServe(cfg.AppConfig.Host+":"+cfg.AppConfig.Port, nil)
+	//err = http.ListenAndServe(cfg.AppConfig.Host+":"+cfg.AppConfig.Port, nil)
 
 	// Для локального тестирования
-	//err = http.ListenAndServe(":5000", nil)
-
+	err = http.ListenAndServe(":8080", nil)
 	slog.Info("Server has started working...")
 
 	if err != nil {
@@ -158,7 +150,7 @@ func newDbConnection(dbConfig *config.DBConfig) (*sql.DB, error) {
 		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Name, dbConfig.SSLMode,
 	)
 
-	db, err := sql.Open("postgres", connectionString)
+	db, err := sql.Open("pgx", connectionString)
 	if err != nil {
 		return nil, e.Wrap(op, err)
 	}
@@ -187,70 +179,38 @@ func runMigrations(db *sql.DB, migrationsDir string) error {
 
 	slog.Info("Applying migrations...")
 	err = m.Up()
-	switch {
-	case err == nil:
-		slog.Info("Migrations are successfully applied")
-		return nil
-	case errors.Is(err, migrate.ErrNoChange):
-		slog.Info("Migrations are already successfully applied")
-		return nil
-	case errors.As(err, new(*migrate.ErrDirty)):
-		version, dirty, vErr := m.Version()
-		if vErr != nil {
-			return e.Wrap(op, vErr)
-		}
-		if dirty {
-			forceVersion := int(version)
-			if forceVersion > 0 {
-				forceVersion--
-			}
-
-			slog.Warn("Detected dirty migration state, forcing database version", "current_version", version, "force_version", forceVersion)
-			if forceErr := m.Force(forceVersion); forceErr != nil {
-				return e.Wrap(op, forceErr)
-			}
-
-			if retryErr := m.Up(); retryErr != nil && !errors.Is(retryErr, migrate.ErrNoChange) {
-				return e.Wrap(op, retryErr)
-			}
-
-			slog.Info("Migrations are successfully applied after resolving dirty state")
-			return nil
-		}
-		return nil
-	default:
+	if err != nil {
 		return e.Wrap(op, err)
 	}
+
+	if errors.Is(err, migrate.ErrNoChange) {
+		slog.Info("Migrations are already successfully applied")
+	} else {
+		slog.Info("Migrations are successfully applied")
+	}
+
+	return nil
 }
 
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 
-	replaceAttrFunc := func(groups []string, a slog.Attr) slog.Attr {
-		if a.Key == slog.TimeKey && len(groups) == 0 {
-			if t, ok := a.Value.Any().(time.Time); ok {
-				a.Value = slog.StringValue(t.Format("2006/01/02 15:04:05"))
-			}
-		}
-		return a
-	}
-
 	switch env {
 	case envLocal:
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug, ReplaceAttr: replaceAttrFunc}),
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
 	case envDev:
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug, ReplaceAttr: replaceAttrFunc}),
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
 		)
 	case envProd:
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo, ReplaceAttr: replaceAttrFunc}),
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
-	default:
+	default: // If env config is invalid, set prod settings by default due to security
 		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo, ReplaceAttr: replaceAttrFunc}),
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
 	}
 
@@ -274,30 +234,5 @@ func newMinioConnection(endpoint, accessKey, secretKey string, useSSL bool) (*mi
 		slog.Warn("Could not connect to MinIO: " + err.Error())
 	}
 
-	slog.Info("Connected to MinIO successfully")
-
 	return minioClient, nil
-}
-
-func bucketExists(client *minio.Client, bucketName string) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	exists, err := client.BucketExists(ctx, bucketName)
-	if err != nil {
-		return err
-	}
-
-	if !exists {
-		slog.Info("Creating bucket...", "bucket", bucketName)
-		err = client.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to create bucket %s: %w", bucketName, err)
-		}
-		slog.Info("Bucket created successfully", "bucket", bucketName)
-	} else {
-		slog.Info("Bucket already exists", "bucket", bucketName)
-	}
-
-	return nil
 }
