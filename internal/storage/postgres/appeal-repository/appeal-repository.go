9abbe11a785317a5f700773e9ba752f41d2af88d
gettingthingsdoneee <wrjmll@gmail.com>
2025-11-15 14:35:08 +0300

    save appeal repository method added

--- internal/storage/postgres/appeal-repository/appeal-repository.go
+++ internal/storage/postgres/appeal-repository/appeal-repository.go
@@ -27,7 +27,36 @@ func (repo *AppealRepository) FindByProfileIDWithKeysetPagination(
 	const op = "storage.appealRepository.FindByProfileIDWithKeysetPagination"
 	log := logger.GetLogger(ctx).With(slog.String("op", op))
 
-	const query = ``
+	const query = `
+		SELECT
+            a.id, a.topic, a.text, a.appeal_status,
+			a.created_at, a.updated_at
+        FROM
+            appeal a
+        LEFT JOIN
+            folder f ON fpm.folder_id = f.id AND f.profile_id = recipient_profile.id
+        JOIN
+            base_profile bp ON m.sender_base_profile_id = bp.id
+        LEFT JOIN
+            profile sender_profile ON bp.id = sender_profile.base_profile_id
+        WHERE
+            recipient_profile.base_profile_id = $1
+			AND (($2 = 0 AND $3 = 0) OR (m.date_of_dispatch, m.id) < (to_timestamp($3), $2))
+        GROUP BY 
+            m.id, 
+            m.topic, 
+            m.text, 
+            m.date_of_dispatch,
+            pm.read_status,
+            bp.id, 
+            bp.username, 
+            bp.domain,
+            sender_profile.name, 
+            sender_profile.surname, 
+            sender_profile.image_path
+        ORDER BY
+            m.date_of_dispatch DESC, m.id DESC
+		FETCH FIRST $4 ROWS ONLY`
 
 	stmt, err := repo.db.PrepareContext(ctx, query)
 	if err != nil {
@@ -64,5 +93,19 @@ func (repo *AppealRepository) FindByProfileIDWithKeysetPagination(
 }
 
 func (repo *AppealRepository) SaveAppeal(ctx context.Context, profileID int64, topic, text string) error {
+	const op = "storage.appealRepository.SaveAppeal"
+	log := logger.GetLogger(ctx).With(slog.String("op", op))
+
+	const query = `
+		INSERT INTO appeal (topic, text, profile_id)
+		VALUES ($1, $2, $3)`
+
+	log.Debug("Execute SaveAppeal query...")
+
+	_, err := repo.db.ExecContext(ctx, query, topic, text, profileID)
+	if err != nil {
+		return e.Wrap(op, err)
+	}
+
 	return nil
 }
