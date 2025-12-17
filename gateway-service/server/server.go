c4632c9a824690ae1d8805360ab84b605a35e810
niggumin <minecrafffft.4lyf@gmail.com>
2025-12-17 18:55:23 +0300

    ci

--- gateway-service/server/server.go
+++ gateway-service/server/server.go
@@ -1219,3 +1219,4 @@ func (s *Server) addTokenToContext(ctx context.Context, token string) context.Co
 	md := metadata.Pairs("authorization", "Bearer "+token)
 	return metadata.NewOutgoingContext(ctx, md)
 }
+
